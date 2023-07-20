893d78cfa45fc80843afc05deba0046de547a5c9
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2023-07-19 19:05:52 -0700

    added transition

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script setup>
-    import {RouterView} from "vue-router"
-    import NavBar from "./components/NavBar.vue"
+import { RouterView } from "vue-router"
+import NavBar from "./components/NavBar.vue"
 </script>
 
 <template>
-        <NavBar />
-    <RouterView />
+    <NavBar />
+    <router-view v-slot="{ Component }">
+        <transition name="fade" mode="out-in">
+            <component :is="Component" />
+        </transition>
+    </router-view>
 </template>
 
 <style scoped>
-
+    .fade-enter-from,
+    .fade-leave-to {
+        opacity: 0;
+    }
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 0.5s ease-out;
+    }
 </style>
